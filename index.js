a903458a14f07eeff667c8485df14e81b54f30f7
Justin Hernandez <justin@circavictor.com>
2019-05-20 22:22:59 -0700

    check for empty cell

--- index.js
+++ index.js
@@ -412,10 +412,12 @@ var SpreadsheetWorksheet = function( spreadsheet, data ){
     self.resize({rowCount: 1, colCount: 1}, function(err) {
       if (err) return cb(err);
       self.getCells(function(err, cells) {
-        cells[0].setValue(null, function(err) {
-          if (err) return cb(err);
-          self.resize({rowCount: rows, colCount: cols}, cb);
-        });
+        if (cells[0]) {
+          cells[0].setValue(null, function(err) {
+            if (err) return cb(err);
+            self.resize({rowCount: rows, colCount: cols}, cb);
+          });
+        }
       })
     });
   }
